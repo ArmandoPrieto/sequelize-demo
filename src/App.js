b8cc9085e5a44e774df27aa129541a7aad5c75b3
ArmandoPrieto <prieto.arm@gmail.com>
2019-01-26 12:10:50 -0800

    Innitial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
 class App extends Component {
+  init(){
+    fetch('http://localhost:3000/api/users')
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(myJson);
+      });
+      let data = {name: 'Ruben Dario'};
+      fetch('http://localhost:3000/api/users', {
+            method: 'POST', // or 'PUT'
+            body: JSON.stringify(data), // data can be `string` or {object}!
+            headers:{
+              'Content-Type': 'application/json'
+            }
+        })
+          .then(response => {
+            if (response.status === 200) {
+              return response.json();
+            } else {
+              throw new Error('Something went wrong on api server!');
+            }
+          })
+          .then(response => {
+            console.debug(response);
+            // ...
+          }).catch(error => {
+            console.error(error);
+          });
+  }
   render() {
+    this.init();
     return (
       <div className="App">
         <header className="App-header">
